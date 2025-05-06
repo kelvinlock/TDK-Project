6459ea85218053c2bb1ee7300ab5b52b91cd7ed6
Kelvin <kelvinlock0714@gmail.com>
2025-05-07 02:19:01 +0800

    Laptop Commit

--- 0507 Axes/Xbox_Dc_Motor/Xbox_Dc_Motor.ino
+++ 0507 Axes/Xbox_Dc_Motor/Xbox_Dc_Motor.ino
@@ -4,13 +4,16 @@ struct Motor {
   int backwardPin;  // 馬達反轉引腳
   int speedPin;     // 馬達使能引腳（控制速度）
 };
-
+// Declare pin number
 Motor motors[4] = {
   {2, 3, 6},    // 馬達1：forwardPin=2, backwardPin=3, enablePin=6
   {4, 5, 6},    // 馬達2：forwardPin=4, backwardPin=5, enablePin=6
   {7, 9, 6},    // 馬達3：forwardPin=7, backwardPin=9, enablePin=6
   {8, 10, 6}    // 馬達4：forwardPin=8, backwardPin=10, enablePin=6
 }
+
+String axes[2];
+int axes_values[2];
 void setup() {
   // 初始化串行通訊S
   Serial.begin(9600);
@@ -25,13 +28,21 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    
-    // 解析數據格式 "PWM:255"
-    if (data.startsWith("X:")) {
-      int speed = data.substring(4).toInt();
-      speed = constrain(speed, 0, 255);  // 確保值在合法範圍
+    String data = Serial.readStringUntil('\n'); // 例如輸入：X:132,Y:234
 
+    if (data.startsWith("X:")) {
+      int comma_index = data.indexOf(",");
+
+      if (comma_index != -1) {
+        axes[0] = data.substring(0, comma_index);           // "X:132"
+        axes[1] = data.substring(comma_index + 1);          // "Y:234"
+
+        for (int i = 0; i < 2; i++) {
+          axes_values[i] = axes[i].substring(2).toInt();    // 取 "132"、"234"
+          axes_values[i] = constrain(axes_values[i], 0, 255);
+        }
+      }
+    }
       // 強化停止判斷（127±5為死區）
       if (speed >= 132) {         // 前進
         digitalWrite(IN1, HIGH);
@@ -50,4 +61,4 @@ void loop() {
   }
 }
 
-void move(int[])
+void move(int, int)
